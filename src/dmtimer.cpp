be75f71814608c9fa5a890b89a8cb05724c4ba03
Nicolas Simonnet <ns@epithelix.com>
2021-02-01 14:50:24 +0100

    this version takes micros() in account and needs to be tested

--- src/dmtimer.cpp
+++ src/dmtimer.cpp
@@ -3,23 +3,64 @@
 /**
 * Constructor
 */
-DMTimer::DMTimer(){
-
+DMTimer::DMTimer()
+{
 }
 
-DMTimer::DMTimer(unsigned long interval){
+DMTimer::DMTimer(unsigned long interval)
+{
   setInterval(interval);
 }
 
-bool DMTimer::isTimeReached(unsigned long currentTime){
+bool DMTimer::isTimeReached(unsigned long currentTime)
+{
   return isTimeReached(currentTime, this->_interval);
 }
 
-bool DMTimer::isTimeReached(unsigned long currentTime, unsigned long interval){
-  if(currentTime > _lastTime + interval)
+bool DMTimer::isTimeReached(unsigned long currentTime, unsigned long interval)
+{
+  if (!_isNextTimeAfterOverflow) //easy case
   {
-    _lastTime = currentTime;
-    return true;
+    if (currentTime >= _triggerTime)
+    {
+      setTriggerTime(_triggerTime + interval);
+      return true;
+    }
+  }
+  else //next trigger time will be is after micros() overflow
+  {
+    //calculate the idle time window
+    unsigned long maxInteger = 0;
+    maxInteger--;
+    unsigned long from = maxInteger - interval;
+    unsigned long to = maxInteger;
+
+    if(currentTime > from && currentTime <= to)
+    {
+      return false;
+    }
+    else
+    {
+      if(currentTime >= _triggerTime)
+      {
+        setTriggerTime(_triggerTime + interval);
+        return true;
+      }
+    }
   }
   return false;
+}
+
+void DMTimer::setInterval(unsigned long interval)
+{
+  this->_interval = interval;
+  this->_triggerTime = micros() + interval;
+}
+
+void DMTimer::setTriggerTime(unsigned long time)
+{
+  //is next time before the current next time ? if so, we'll consider the overflow of micros()
+  this->_isNextTimeAfterOverflow = time < this->_triggerTime;
+
+  this->_triggerTime = time;
 }
--- src/dmtimer.h
+++ src/dmtimer.h
@@ -3,26 +3,32 @@
 
 #include "Arduino.h"
 
-class DMTimer {
+class DMTimer
+{
 private: //private members
-
-public: //public members
+public:  //public members
   //DMTimer constructor
   DMTimer();
   DMTimer(unsigned long interval);
 
+  //indicates if next trigger time has been reached
   bool isTimeReached(unsigned long currentTime, unsigned long interval);
   bool isTimeReached(unsigned long currentTime);
   bool isTimeReached() { return isTimeReached(micros()); }
 
-  void reset() { setLastTime(micros()); }
-  void setLastTime(unsigned long time) { this->_lastTime = time; }
-  void setInterval(unsigned long interval) { this->_interval = interval; }
+  //resets next trigger time to micros() + interval
+  void reset() { setTriggerTime(micros() + _interval); }
+
+  void setTriggerTime(unsigned long time);
+  
+  void setInterval(unsigned long interval);
 
 private:
-  unsigned long _lastTime = 0;
+  unsigned long _triggerTime = 0;
   unsigned long _interval = 0;
 
+  //indicates if the next trigger time is planned after micros() overflow.
+  bool _isNextTimeAfterOverflow;
 };
 
 #endif //DMTIMER_H
